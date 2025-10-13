adf37df7de3ff39852fb5ab79ba4e10421d4dc59
DaniilGladskikh <slysaa@mail.ru>
2025-10-14 02:15:46 +0500

    1_8_3 Fix a broken story tray

--- src/1/1_8_3/App.tsx
+++ src/1/1_8_3/App.tsx
@@ -1,9 +1,4 @@
-// 1_8_3 Fix a broken story tray
-/*
-  Генеральный директор вашей компании просит вас добавить "истории" в ваше приложение онлайн-часов, и вы не можете отказать. Вы написали компонент StoryTray, который принимает список stories, за которым следует заполнитель "Create Story".
-
-  Вы реализовали заполнитель "Create Story", поместив еще одну фальшивую историю в конец массива stories, который вы получаете в качестве пропса. Но по какой-то причине "Create Story" появляется более одного раза. Исправьте эту проблему.
-*/
+// 1_8_3 Ошибка была в том, что компонент StoryTray мутировал входной массив stories, добавляя элемент "Create Story" с помощью метода push. Это приводило к добавлению элемента при каждом рендере. Исправление заключалось в создании нового массива с помощью метода concat, который объединяет оригинальный массив историй с дополнительным элементом, не мутируя оригинальный массив.
 
 export interface Story {
   id: string;
@@ -16,16 +11,16 @@ export default function Wrapper(){
 }
 
  function StoryTray({ stories }: { stories: Story[] }) {
-  stories.push({
-      id: 'create',
-      label: 'Create Story',
-  });
+   const storiesWithCreate = stories.concat({
+       id: 'create',
+       label: 'Create Story',
+   });
 
-  return (
-      <ul>
-          {stories.map((story) => (
-              <li key={story.id}>{story.label}</li>
-          ))}
-      </ul>
-  );
+   return (
+       <ul>
+           {storiesWithCreate.map((story) => (
+               <li key={story.id}>{story.label}</li>
+           ))}
+       </ul>
+   );
 }
