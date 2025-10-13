546c03b5a2903855bc89073140d2f2634ec897b0
DaniilGladskikh <slysaa@mail.ru>
2025-10-14 00:55:56 +0500

    1_7_3 Extracting a list item component

--- src/1/1_7_3/App.tsx
+++ src/1/1_7_3/App.tsx
@@ -1,27 +1,39 @@
-// 1_7_3 Extracting a list item component 
-/*
-  Этот компонент RecipeList содержит два вложенных вызова map. Чтобы упростить его, извлеките из него компонент Recipe, который будет принимать пропсы id, name и ingredients. Где вы разместите внешний key и почему?.
-*/
+// 1_7_3 Был извлечен компонент Recipe, который принимает пропсы id, name и ingredients. Внешний key размещен в компоненте RecipeList при вызове map, потому что React требует уникальный ключ для каждого элемента в списке. Key должен находиться на верхнем уровне элемента списка, а не внутри компонента Recipe, чтобы React мог правильно отслеживать изменения в списке.
 
 import { recipes } from './data';
+
+type RecipeProps = {
+    id: string;
+    name: string;
+    ingredients: string[];
+}
+
+function Recipe({ id, name, ingredients }: RecipeProps) {
+    return (
+        <div key={id}>
+            <h2>{name}</h2>
+            <ul>
+                {ingredients.map((ingredient) => (
+                    <li key={ingredient}>
+                        {ingredient}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 export default function RecipeList() {
     return (
         <div>
             <h1>Recipes</h1>
             {recipes.map((recipe) => (
-                <div key={recipe.id}>
-                    <h2>{recipe.name}</h2>
-                    <ul>
-                        {recipe.ingredients.map(
-                            (ingredient) => (
-                                <li key={ingredient}>
-                                    {ingredient}
-                                </li>
-                            )
-                        )}
-                    </ul>
-                </div>
+                <Recipe
+                    key={recipe.id}
+                    id={recipe.id}
+                    name={recipe.name}
+                    ingredients={recipe.ingredients}
+                />
             ))}
         </div>
     );
