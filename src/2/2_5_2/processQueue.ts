7b94a8f2d6cb1ddd23e761c96da4c6a7a09b4e17
DaniilGladskikh <slysaa@mail.ru>
2025-10-14 20:00:03 +0500

    2_5_2 Implement the state queue yourself

--- src/2/2_5_2/processQueue.ts
+++ src/2/2_5_2/processQueue.ts
@@ -1,9 +1,21 @@
+/*
+  Функция getFinalState обрабатывает очередь обновлений состояния, как это делает React.
+  Если элемент очереди - функция, она применяется к текущему состоянию.
+  Если элемент очереди - значение, оно заменяет текущее состояние.
+  Обработка производится последовательно для каждого элемента в очереди.
+*/
 import { Que } from "./App";
 
 export function getFinalState(baseState: number, queue: Que[]) {
     let finalState = baseState;
 
-    // TODO: do something with the queue...
+    for (const update of queue) {
+        if (typeof update === 'function') {
+            finalState = update(finalState);
+        } else {
+            finalState = update;
+        }
+    }
 
     return finalState;
 }
