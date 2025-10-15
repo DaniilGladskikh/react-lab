e00ca9456a14d8954aab7f406e0bc1f15ed7e479
DaniilGladskikh <slysaa@mail.ru>
2025-10-15 17:29:59 +0500

    3_1_3 Refactor the imperative solution without React

--- index/3_1_3/index.js
+++ index/3_1_3/index.js
@@ -1,53 +1,72 @@
+let state = {
+    isEditing: false,
+    firstName: 'Jane',
+    lastName: 'Jacobs',
+};
+
+function updateDOM() {
+    if (state.isEditing) {
+        editButton.textContent = 'Save Profile';
+        hide(firstNameText);
+        hide(lastNameText);
+        show(firstNameInput);
+        show(lastNameInput);
+    } else {
+        editButton.textContent = 'Edit Profile';
+        hide(firstNameInput);
+        hide(lastNameInput);
+        show(firstNameText);
+        show(lastNameText);
+    }
+    firstNameInput.value = state.firstName;
+    lastNameInput.value = state.lastName;
+    firstNameText.textContent = state.firstName;
+    lastNameText.textContent = state.lastName;
+    helloText.textContent = 'Hello, ' + state.firstName + ' ' + state.lastName + '!';
+}
+
 function handleFormSubmit(e) {
     e.preventDefault();
-    if (editButton.textContent === 'Edit Profile') {
-      editButton.textContent = 'Save Profile';
-      hide(firstNameText);
-      hide(lastNameText);
-      show(firstNameInput);
-      show(lastNameInput);
-    } else {
-      editButton.textContent = 'Edit Profile';
-      hide(firstNameInput);
-      hide(lastNameInput);
-      show(firstNameText);
-      show(lastNameText);
-    }
-  }
-  
-  function handleFirstNameChange() {
-    firstNameText.textContent = firstNameInput.value;
-    helloText.textContent = (
-      'Hello ' +
-      firstNameInput.value + ' ' +
-      lastNameInput.value + '!'
-    );
-  }
-  
-  function handleLastNameChange() {
-    lastNameText.textContent = lastNameInput.value;
-    helloText.textContent = (
-      'Hello ' +
-      firstNameInput.value + ' ' +
-      lastNameInput.value + '!'
-    );
-  }
-  
-  function hide(el) {
+    setState({
+        isEditing: !state.isEditing,
+    });
+}
+
+function handleFirstNameChange() {
+    setState({
+        firstName: firstNameInput.value,
+    });
+}
+
+function handleLastNameChange() {
+    setState({
+        lastName: lastNameInput.value,
+    });
+}
+
+function hide(el) {
     el.style.display = 'none';
-  }
-  
-  function show(el) {
+}
+
+function show(el) {
     el.style.display = '';
-  }
-  
-  let form = document.getElementById('form');
-  let editButton = document.getElementById('editButton');
-  let firstNameInput = document.getElementById('firstNameInput');
-  let firstNameText = document.getElementById('firstNameText');
-  let lastNameInput = document.getElementById('lastNameInput');
-  let lastNameText = document.getElementById('lastNameText');
-  let helloText = document.getElementById('helloText');
-  form.onsubmit = handleFormSubmit;
-  firstNameInput.oninput = handleFirstNameChange;
-  lastNameInput.oninput = handleLastNameChange;
+}
+
+function setState(newState) {
+    state = { ...state, ...newState };
+    updateDOM();
+}
+
+let form = document.getElementById('form');
+let editButton = document.getElementById('editButton');
+let firstNameInput = document.getElementById('firstNameInput');
+let firstNameText = document.getElementById('firstNameText');
+let lastNameInput = document.getElementById('lastNameInput');
+let lastNameText = document.getElementById('lastNameText');
+let helloText = document.getElementById('helloText');
+
+form.onsubmit = handleFormSubmit;
+firstNameInput.oninput = handleFirstNameChange;
+lastNameInput.oninput = handleLastNameChange;
+
+updateDOM();
