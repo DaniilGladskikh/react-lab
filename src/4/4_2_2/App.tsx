8878dd1c892a373ee8586a3c11bf727b81972589
DaniilGladskikh <slysaa@mail.ru>
2025-10-16 16:13:32 +0500

    4_2_2 Focus the search field

--- src/4/4_2_2/App.tsx
+++ src/4/4_2_2/App.tsx
@@ -1,15 +1,17 @@
-// 4_2_2 Focus the search field
-/*
-  Сделайте так, чтобы нажатие на кнопку "Поиск" наводило фокус на поле.
-*/
+// 4_2_2 Добавлен фокус на поле ввода при нажатии на кнопку "Поиск". Использован useRef для получения доступа к элементу input и вызова его метода focus().
+
+import { useRef } from 'react';
 
 export default function Page() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button onClick={() => inputRef.current?.focus()}>Search</button>
       </nav>
       <input
+        ref={inputRef}
         placeholder="Looking for something?"
       />
     </>
