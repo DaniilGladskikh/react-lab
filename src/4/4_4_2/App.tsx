5ebbe177386c61d1faf9b9c265464bf131d24989
DaniilGladskikh <slysaa@mail.ru>
2025-10-16 19:21:22 +0500

    4_4_2 Cache a calculation without Effects

--- src/4/4_4_2/App.tsx
+++ src/4/4_4_2/App.tsx
@@ -1,22 +1,13 @@
-// 4_4_2 Cache a calculation without Effects 
-/*
-  В этом примере фильтрация тодосов была вынесена в отдельную функцию под названием getVisibleTodos(). Эта функция содержит внутри себя вызов console.log(), который поможет вам заметить, когда она вызывается. Установите флажок "Показывать только активные тодосы" и обратите внимание, что это вызывает повторный запуск getVisibleTodos(). Это ожидаемо, поскольку видимые тодосы меняются, когда вы переключаете, какие из них показывать.
+// 4_4_2 Удален useEffect для visibleTodos и добавлен useMemo для кэширования результата функции getVisibleTodos(). Это предотвращает повторный запуск getVisibleTodos() при вводе текста в input, но позволяет пересчитывать список при изменении todos или showActive.
 
-  Ваша задача - удалить эффект, который пересчитывает список visibleTodos в компоненте TodoList. Однако, вам нужно убедиться, что getVisibleTodos() не повторно запускается (и поэтому не печатает никаких логов), когда вы вводите данные в input.
-*/
-
-import { useState, useEffect } from 'react';
-import { initialTodos, createTodo, getVisibleTodos, Todo } from './todos.js';
+import { useState, useMemo } from 'react';
+import { initialTodos, createTodo, getVisibleTodos } from './todos.js';
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
   const [showActive, setShowActive] = useState(false);
   const [text, setText] = useState('');
-  const [visibleTodos, setVisibleTodos] = useState<Todo[]>([]);
-
-  useEffect(() => {
-    setVisibleTodos(getVisibleTodos(todos, showActive));
-  }, [todos, showActive]);
+  const visibleTodos = useMemo(() => getVisibleTodos(todos, showActive), [todos, showActive]);
 
   function handleAddClick() {
     setText('');
